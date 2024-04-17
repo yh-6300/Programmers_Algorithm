a12a67cf1a888f7708330e8036e46b2b42017f25
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-17 20:46:26 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    for(int i=0;i<myString.length();i++){
+        if(myString[i] == 'A'){
+            myString[i] = 'B';
+        }
+        else if(myString[i] == 'B'){
+            myString[i] = 'A';
+        }
+    }
+    
+    if(myString.find(pat) != string::npos){
+        answer = 1;
+    }
+    
+    
+    return answer;
+}
