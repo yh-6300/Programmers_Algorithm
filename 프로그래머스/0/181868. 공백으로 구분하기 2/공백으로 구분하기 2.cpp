8c6e451abbd2fc23314f8c95f109645c4ac24186
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-18 19:00:40 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <typeinfo>
+#include <sstream>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    stringstream ss(my_string);
+    
+    string s;
+    while (ss >> s)
+    {
+        answer.push_back(s);
+    }
+    
+    return answer;
+}
