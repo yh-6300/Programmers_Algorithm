e487decb810f110625124db9982b662234ff7027
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-20 22:38:27 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2) {
+    int answer = 0;
+    
+    for(int i = 0; i < date1.size(); ++i) {
+        if(date1[i] > date2[i]) return 0;
+        else if(date1[i] < date2[i]) return 1;
+    }
+    
+    return answer;
+}
