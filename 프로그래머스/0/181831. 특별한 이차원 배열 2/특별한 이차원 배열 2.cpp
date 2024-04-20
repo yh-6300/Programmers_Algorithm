cc5dce6e274c7d78fc83090601610441dcfee556
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-20 18:42:57 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> arr) {
+    int answer = 1;
+    
+    for(int i=0;i<arr.size();i++){
+        for(int j=0;j<arr[0].size();j++){
+            if(arr[i][j] != arr[j][i])
+                return 0;
+        }
+    }
+    
+    
+    return answer;
+}
