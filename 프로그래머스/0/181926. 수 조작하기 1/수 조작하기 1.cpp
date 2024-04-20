69c2c26617c6e9f0d1bda63bdee0e6cd0d3bc395
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-20 17:58:30 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.16 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = 0;
+    
+    for(int i=0;i<control.length();i++){
+        if(control[i] == 'w'){
+            n += 1;
+        }
+        else if(control[i] == 's'){
+            n -= 1;
+        }
+        else if(control[i] == 'd'){
+            n += 10;
+        }
+        else if(control[i] == 'a'){
+            n -= 10;
+        }
+    }
+    return n;
+}
