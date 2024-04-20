80a52b54393d07ff64458154cdd41b3a66f51a1d
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-20 18:49:55 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    int answer = 0;
+    
+    for(int i=0;i<num_list.size();i++){
+        if(num_list[i] == n)
+            return 1;
+    }
+    
+    
+    return answer;
+}
