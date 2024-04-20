784bb1ba1f283ea186e6a5404c0f23a3cd8c5aa8
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-20 18:47:31 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string n_str) {
+    string answer = "";
+    
+    for(int i=0;i<n_str.length();i++){
+        if(n_str[i] != '0'){
+            answer = n_str.substr(i,n_str.length()-i);
+            return answer;
+        }
+    }
+    return answer;
+}
