3d1eb3747521694bee9ef6833c9cc33a59c49fca
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-20 21:56:03 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string number) {
+    int answer = 0;
+    
+    for(int i=0;i<number.length();i++){
+        answer += number[i] - '0';
+    }
+    
+    return answer%9;
+}
