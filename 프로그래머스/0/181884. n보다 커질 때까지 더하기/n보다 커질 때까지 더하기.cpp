bd5bf44347326ca473ba81ea1c1c1f20ef6fea46
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-20 17:13:19 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int n) {
+    int answer = 0;
+    
+    for(int i=0;i<numbers.size();i++){
+        if(answer > n){
+            return answer;
+        }
+        else{
+            answer += numbers[i];
+        }
+    }
+    
+    return answer;
+}
