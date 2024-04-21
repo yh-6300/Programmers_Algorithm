9d01055e97c0ae17e0c3f158a1091a9fcf5b257e
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-21 18:48:27 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    
+    answer = my_string.substr(0,s) + overwrite_string 
+        + my_string.substr(s+overwrite_string.length(),my_string.length()-(s+overwrite_string.length()));
+    
+    return answer;
+}
