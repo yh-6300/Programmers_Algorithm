7c09cb5345f78c00a13427cf9925337753aea3da
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-22 21:30:21 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    
+    string s = to_string(x);
+    int sum = 0;
+    for(int i=0;i<s.length();i++){
+        sum += s[i]-'0';
+    }
+    
+    if(x%sum != 0){
+        answer = false;
+    }
+    
+    return answer;
+}
