6cfe5bbfc4d770530dde8a241235afc848d82aa1
YeoHyeon <85444655+yh-6300@users.noreply.github.com>
2024-04-22 21:18:38 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    
+    string s = to_string(n);
+    for(int i=0;i<s.length();i++){
+        answer += s[i]-'0';
+    }
+
+    return answer;
+}
